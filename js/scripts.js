ae2b8703b3901dd53bd23d9df3d8421ec052d097
yveswangui <yveswangui@gmail.com>
2022-01-28 00:31:04 +0300

    update calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
+//Business Logic
+
 var add = function(number1,number2){
+  return number1 + number2;
+};
+var subtract = function(number1, number2){
+  return number1 - number2;
+};
+var multiply = function(number1, number2){
   return number1 * number2;
 };
-var number1 = parseInt(prompt("Enter a number:"))
-var number2 = parseInt(prompt("Enter another number:"))
-var result = add(number1 ,number2);
-alert(result);
+var divide = function(number1, number2){
+  return number1 / number2;
+};
+
+//user interface
+$(document).ready(function(){
+  $("form#add").submit(function(event){
+    event.preventDefault();
+
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1,number2);
+    $("#output").text(result)
+  });
+});
+
 
 
